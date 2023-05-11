4a4abeca9ae8dfac28d944ecdb16e86f92989120
Saloni <salonijain1248@gmail.com>
2023-05-11 23:24:42 +1000

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+mongoose.connect('mongodb+srv://karan:karan@cluster0.gfuxd.mongodb.net/?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 
